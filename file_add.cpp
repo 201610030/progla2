23a0b58cd3f76b3f87d5802ee42c49cb338c9c50
Bryan Joshua B. Edejer <bryan.joshua.edejer@gmail.com>
2017-01-07 17:40:43 +0800

    Create file_add.cpp

---
+++ file_add.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    FILE *myFile;
+    myFile = fopen("sample.txt", "r");
+
+    int i;
+    int numberArray[5];
+    int sum = 0;
+
+    for (i = 0; i < 5; i++){
+        fscanf(myFile, "%d", &numberArray[i]);
+    }
+
+    for (i = 0; i < 5 ; i++){
+    	sum += numberArray[i]; 
+	}
+	
+	cout << "The sum of all numbers is: " << sum;
+	
+	return 0;
+}
